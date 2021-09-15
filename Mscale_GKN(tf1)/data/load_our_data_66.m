84d4de72a623df0823424fb85745fa0bebd6e3c7
Blue-Giant <42233512+Blue-Giant@users.noreply.github.com>
2021-09-15 10:36:17 +0800

    Add files via upload

---
+++ Mscale_GKN(tf1)/data/load_our_data_66.m
@@ -0,0 +1,39 @@
+clc
+clear all
+close all
+data = load('res66_a4_f1.mat');
+ceof = data.meshA;
+ACE = reshape(ceof(10,:,:),[66,66]);
+New_ACE = reshape(ACE, [1,66*66]);
+
+solu = data.meshU;
+Asolu = reshape(solu(1,:,:),[66,66]); 
+
+XYpoints = data.meshXY;
+Xcoords = XYpoints(1,:);
+Ycoords = XYpoints(2,:);
+X = reshape(Xcoords, [66, 66]);
+Y = reshape(Ycoords, [66, 66]);
+
+figure('name','meshU')
+surf(X, Y, Asolu)
+hold on
+
+figure('name', 'meshA')
+surf(X, Y, ACE)
+hold on
+
+% figure('name', 'log_meshA')
+% surf(X, Y, log(ACE))
+% hold on
+
+% πÈ“ªªØAeps
+mean_value = mean(mean(ACE));
+std_value = std2(ACE);
+
+Gauss_ACE = (ACE-mean_value)./(std_value*std_value);
+
+figure('name', 'Normalize_meshA')
+surf(X, Y, Gauss_ACE)
+hold on
+
