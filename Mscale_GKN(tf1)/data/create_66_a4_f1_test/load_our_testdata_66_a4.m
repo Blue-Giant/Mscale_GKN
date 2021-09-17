d650e843527d5be3939dcb534b48369dcd2a259d
Blue-Giant <42233512+Blue-Giant@users.noreply.github.com>
2021-09-17 09:42:56 +0800

    Add files via upload

---
+++ Mscale_GKN(tf1)/data/create_66_a4_f1_test/load_our_testdata_66_a4.m
@@ -0,0 +1,36 @@
+clc
+clear all
+close all
+N = 66;
+data = load('res66_a4_f1_test.mat');
+ceof = data.meshA;
+solu = data.meshU; 
+
+XYpoints = data.meshXY;
+Xcoords = XYpoints(1,:);
+Ycoords = XYpoints(2,:);
+X = reshape(Xcoords, [N, N]);
+Y = reshape(Ycoords, [N, N]);
+
+figure('name','meshU')
+surf(X, Y, solu)
+hold on
+
+figure('name', 'meshA')
+surf(X, Y, ceof)
+hold on
+
+% figure('name', 'log_meshA')
+% surf(X, Y, log(ACE))
+% hold on
+
+% πÈ“ªªØAeps
+mean_value = mean(mean(ceof));
+std_value = std2(ceof);
+
+Gauss_ceof = (ceof-mean_value)./(std_value*std_value+5.0);
+
+figure('name', 'Normalize_meshA')
+surf(X, Y, Gauss_ceof)
+hold on
+
---
+++ Mscale_GKN(tf1)/data/create_res130_a4_f1_test/create_test_index130.m
@@ -0,0 +1,12 @@
+clc;
+clear all
+close all
+q = 7;
+N = 2^q+1;
+M = N+1;
+
+original_index = int32(linspace(0,M*M-1,M*M));
+disorder_index = int32(reorder_index(original_index, M));
+
+
+save('disorder_index130.mat','disorder_index');
